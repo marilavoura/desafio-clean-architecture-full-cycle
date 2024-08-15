367e277504b2f8a6388e3d802502ba47f8e9f5fc
marianalavoura <marianalavoura@unesp.br>
2024-08-15 17:41:25 -0300

    first commit

---
+++ internal/entity/interface.go
@@ -0,0 +1,7 @@
+package entity
+
+type OrderRepositoryInterface interface {
+	Save(order *Order) error
+	GetOrders() ([]Order, error)
+	// GetTotal() (int, error)
+}
---
+++ internal/infra/grpc/service/order_service.go
@@ -0,0 +1,60 @@
+package service
+
+import (
+	"context"
+
+	"github.com/devfullcycle/20-CleanArch/internal/infra/grpc/pb"
+	"github.com/devfullcycle/20-CleanArch/internal/usecase"
+)
+
+type OrderService struct {
+	pb.UnimplementedOrderServiceServer
+	CreateOrderUseCase usecase.CreateOrderUseCase
+	ListOrderUseCase   usecase.ListOrdersUseCase
+}
+
+func NewOrderService(createOrderUseCase usecase.CreateOrderUseCase, listOrderUseCase usecase.ListOrdersUseCase) *OrderService {
+	return &OrderService{
+		CreateOrderUseCase: createOrderUseCase,
+		ListOrderUseCase:   listOrderUseCase,
+	}
+}
+
+func (s *OrderService) CreateOrder(ctx context.Context, in *pb.CreateOrderRequest) (*pb.Order, error) {
+	dto := usecase.OrderInputDTO{
+		ID:    in.Id,
+		Price: float64(in.Price),
+		Tax:   float64(in.Tax),
+	}
+	output, err := s.CreateOrderUseCase.Execute(dto)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.Order{
+		Id:         output.ID,
+		Price:      float32(output.Price),
+		Tax:        float32(output.Tax),
+		FinalPrice: float32(output.FinalPrice),
+	}, nil
+}
+
+func (s *OrderService) ListOrders(ctx context.Context, in *pb.Blank) (*pb.ListOrdersResponse, error) {
+	output, err := s.ListOrderUseCase.Execute()
+	if err != nil {
+		return nil, err
+	}
+
+	var orders []*pb.Order
+	for _, order := range output {
+		orders = append(orders, &pb.Order{
+			Id:         order.ID,
+			Price:      float32(order.Price),
+			Tax:        float32(order.Tax),
+			FinalPrice: float32(order.FinalPrice),
+		})
+	}
+
+	return &pb.ListOrdersResponse{
+		Orders: orders,
+	}, nil
+}
